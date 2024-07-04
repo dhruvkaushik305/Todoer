a7d02d735b581ef9b3dbb316a27b270e3b54b5be
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2024-07-04 10:39:58 +0530

    FIX: Solved quote overflow and made the quotes functional

--- frontend/src/Pages/Home/Dashboard/Quote.tsx
+++ frontend/src/Pages/Home/Dashboard/Quote.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { getRandom } from "@anilseervi/inspirational-quotes";
 import { IoRefreshSharp } from "react-icons/io5";
+type QuoteType = {
+  quote: string;
+  author: string;
+};
 const Quote: React.FC = () => {
+  const [quote, setQuote] = React.useState<QuoteType>({} as QuoteType);
+  React.useEffect(() => {
+    setQuote(getRandom());
+  }, []);
   return (
-    <div className="overflow-x-hiddentext-xl flex max-h-32 items-center gap-2 overflow-y-auto italic">
-      <p className="p-2 text-xl italic">
-        "Don't be busy being busy, be busy being done." - Steve Jobs
-      </p>
-      <IoRefreshSharp className="size-15" />
+    <div className="flex h-[4rem] items-center gap-2 text-xl italic">
+      <div className="max-h-[4rem] w-fit overflow-y-auto p-1 text-xl italic">
+        <p>{quote.quote}</p>
+        <p className="text-right text-lg italic">~{quote.author}</p>
+      </div>
+      <IoRefreshSharp
+        className="size-15 cursor-pointer"
+        onClick={() => setQuote(getRandom())}
+      />
     </div>
   );
 };
