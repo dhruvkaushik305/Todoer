44905f599c001e9f5b0fe28e81595bf309e86de1
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2024-07-12 17:00:01 +0530

    STYLE: Added some basic text to the landing page

--- frontend/src/Pages/Landing/LandingLayout.tsx
+++ frontend/src/Pages/Landing/LandingLayout.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 const LandingLayout: React.FC = () => {
-  return <div>This is the landing page</div>;
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <p className="text-3xl font-medium text-gray-600 md:text-4xl">
+        A minimalist todo app
+      </p>
+      <p className="text-md text-gray-500 md:text-xl">
+        Made with love by{" "}
+        <a
+          href="https://github.com/dhruvkaushik305"
+          target="_blank"
+          className="underline"
+        >
+          Dhruv Kaushik
+        </a>
+      </p>
+    </div>
+  );
 };
 export default LandingLayout;
